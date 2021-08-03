7bebbfc3188cf5486652f598431bc71cc926399e
shivam chaurivar <schauriva21@gmail.com>
2021-08-03 14:23:59 +0530

    Create upper_confidence_bound_practise.py

---
+++ Reinforcement Learning/Upper Confidence Bound (UCB)/upper_confidence_bound_practise.py
@@ -0,0 +1,99 @@
+#Importing Lib
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Importing Datasets
+dataset = pd.read_csv('Ads_CTR_Optimisation.csv')
+
+#Implementing Random Selection 
+import random
+N = 10000
+d = 10
+ad_selected = []
+total_reward = 0
+
+for n in range(0, N):
+    ad = random.randrange(d)
+    ad_selected.append(ad)
+    reward = dataset.values[n, ad]
+    total_reward = total_reward + reward
+    
+#ploting Random selection
+plt.hist(ad_selected)
+plt.show()
+    
+
+
+#Implementing Upper Confidence Bound
+import math
+
+
+N = dataset.shape[0]                 #Length of dataset
+d = dataset.shape[1] 
+
+
+ads_selected = []
+numbers_of_selection = [0] * d       #vector of size d with zeros in it
+sums_of_rewards = [0] * d            #vector of size d with zeros in it
+total_reward = 0
+
+for n in range(0, N):
+    max_upper_bound = 0              #Initialize upper bound to find best ad out of 10
+    ad = 0
+    for i in range(0, d):
+        if (numbers_of_selection[i] > 0 ):
+            avg_reward = sums_of_rewards[i] / numbers_of_selection[i]
+            delta_i = math.sqrt(3/2 * math.log(n + 1) / numbers_of_selection[i])
+            upper_bound = avg_reward + delta_i     
+        else:
+            upper_bound = 1e400
+        
+        if upper_bound > max_upper_bound:
+            max_upper_bound = upper_bound
+            ad = i
+        
+    ads_selected.append(ad) 
+    numbers_of_selection[ad] = numbers_of_selection[ad] + 1
+    reward = dataset.values[n, ad]
+    sums_of_rewards[ad] = sums_of_rewards[ad] + reward
+    total_reward = total_reward + reward
+    
+    
+#visualization 
+plt.hist(ads_selected)
+plt.show()
+    
+     
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
